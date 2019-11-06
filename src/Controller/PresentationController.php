e38867247327909133439e69c15c73814a5fafba
Florentin Wilfried <florentinwilfried@gmail.com>
2019-11-06 16:29:43 +0100

    Add presentation page html and css

---
+++ src/Controller/PresentationController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class PresentationController extends AbstractController
+{
+
+    /**
+     * Display presention page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('Page_CCNO/presentation_CCNO.html.twig');
+    }
+}
