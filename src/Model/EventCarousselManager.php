8c413048908f92144eeea74d3ee34e97f9d537e8
wilder <anthonykwanchung@gmail.com>
2019-11-07 17:23:05 +0100

    PR corrections done

--- src/Model/EventCarousselManager.php
+++ src/Model/EventCarousselManager.php
@@ -5,8 +5,10 @@ namespace App\Model;
 
 class EventCarousselManager extends AbstractManager
 {
+    const IMGLIMIT = 3;
     public function selectPictureCaroussel()
     {
-        return $this->pdo->query('SELECT image FROM ' . $this->table . ' WHERE caroussel=1 LIMIT 3;')->fetchAll();
+        return $this->pdo->query('SELECT image FROM ' . $this->table .
+            ' WHERE caroussel=1 LIMIT ' . self::IMGLIMIT . ';')->fetchAll();
     }
 }
