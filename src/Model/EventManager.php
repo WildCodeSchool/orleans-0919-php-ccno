a3de3b747fe8491948914af2dd04406f0505af30
LucasThvn <thauvinlucas@gmail.com>
2019-11-08 14:10:57 +0100

    added date based events

--- src/Model/EventManager.php
+++ src/Model/EventManager.php
@@ -14,6 +14,8 @@ class EventManager extends AbstractManager
     {
         return $this->pdo->query('SELECT image, title, datetime, name FROM ' . $this->table . ' e  
          JOIN category c ON c.id = e.category_id
-         JOIN representation r ON e.id = r.event_id')->fetchAll();
+         JOIN representation r ON e.id = r.event_id
+         ORDER BY datetime
+         LIMIT 6')->fetchAll();
     }
 }
