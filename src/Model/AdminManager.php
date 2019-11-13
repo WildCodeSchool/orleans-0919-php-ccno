0d1bed0947dff9fc7d117c2b487bdae4617201d5
Steven Bédu <steven.bedu@gmail.com>
2019-11-13 16:13:45 +0100

    fix Create for adminPage

--- src/Model/AdminManager.php
+++ src/Model/AdminManager.php
@@ -27,50 +27,6 @@ class AdminManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
-
-    /**
-     * @param array $admin
-     * @return int
-     */
-    public function insert(array $admin): int
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("INSERT INTO $this->table (`title`) VALUES (:title)");
-        $statement->bindValue('title', $admin['title'], \PDO::PARAM_STR);
-
-        if ($statement->execute()) {
-            return (int)$this->pdo->lastInsertId();
-        }
-    }
-
-
-    /**
-     * @param int $id
-     */
-    public function delete(int $id): void
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE event.id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-    }
-
-
-    /**
-     * @param array $admin
-     * @return bool
-     */
-    public function update(array $admin):bool
-    {
-
-        // prepared request
-        $statement = $this->pdo->prepare("UPDATE $this->table SET `title` = :title WHERE id=:id");
-        $statement->bindValue('id', $admin['id'], \PDO::PARAM_INT);
-        $statement->bindValue('title', $admin['title'], \PDO::PARAM_STR);
-
-        return $statement->execute();
-    }
-
     public function selectAllEvents(): array
     {
         return $this->pdo->query('SELECT * FROM ' . $this->table . ' JOIN category c ON c.id = event.category_id
