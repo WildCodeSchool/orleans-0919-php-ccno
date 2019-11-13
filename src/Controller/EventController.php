b96b073a2eb06d1ee994fd4f0074cdd648aff90c
LucasThvn <thauvinlucas@gmail.com>
2019-11-13 10:45:19 +0100

    fix code sniffer

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,13 +3,10 @@
 
 namespace App\Controller;
 
-
 class EventController extends AbstractController
 {
     public function index()
     {
         return $this->twig->render('Event/index.html.twig');
     }
-
-
 }
