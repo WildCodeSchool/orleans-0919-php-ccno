63a076b55ece3b45eb9e5432596e0db95b1052c5
LucasThvn <thauvinlucas@gmail.com>
2019-11-13 10:44:47 +0100

    begining of event page

---
+++ src/Controller/EventController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class EventController extends AbstractController
+{
+    public function index()
+    {
+        return $this->twig->render('Event/index.html.twig');
+    }
+
+
+}
