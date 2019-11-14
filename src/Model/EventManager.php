8dcbe2b92a427bb5d945e7555ad27af6acf47d99
LucasThvn <thauvinlucas@gmail.com>
2019-11-14 21:05:28 +0100

    global event page finished

--- src/Model/EventManager.php
+++ src/Model/EventManager.php
@@ -14,19 +14,30 @@ class EventManager extends AbstractManager
 
     public function showEventHomePage()
     {
-
         return $this->pdo->query('SELECT e.id, image, title, datetime, name , place FROM ' . $this->table . ' e  
          JOIN category c ON c.id = e.category_id
          JOIN representation r ON e.id = r.event_id
+         WHERE datetime > CURDATE()
          ORDER BY datetime
          LIMIT ' . self::NUMBERPICTURE . ';')->fetchAll();
     }
 
     public function showEvent()
     {
+        return $this->pdo->query('
+        SELECT MONTH(datetime) month, YEAR(datetime) year, e.id, image, title, datetime, place, name 
+        FROM ' . $this->table . ' e  
+        JOIN category c ON c.id = e.category_id
+        JOIN representation r ON e.id = r.event_id
+        ORDER BY datetime;')->fetchAll();
+    }
+
+    public function showEventPerMonth($month, $year)
+    {
         return $this->pdo->query('SELECT e.id, image, title, datetime, place, name FROM ' . $this->table . ' e  
          JOIN category c ON c.id = e.category_id
          JOIN representation r ON e.id = r.event_id
+         WHERE MONTH(datetime) = ' . $month . ' AND YEAR(datetime) = ' . $year . '
          ORDER BY datetime;')->fetchAll();
     }
 
