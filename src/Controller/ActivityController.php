df01e0db99bf73e09469664725fbc82e4625e6ec
LucasThvn <thauvinlucas@gmail.com>
2019-11-19 18:25:15 +0100

    une vrai pr.

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use App\Model\ActivityManager;
 use App\Model\EventCarousselManager;
 use App\Model\EventManager;
 
@@ -21,58 +22,29 @@ class ActivityController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function course()
+    public function index(int $type)
     {
-        $activities = [
-        ['public' => 'Pour les enfants',
-        'image' => '../assets/images/dancing_kids.jpg',
-        'title' => 'Cours de danse',
-        'recurrence' => 'Tous les mercredis de Septembre 2019 à juin 2020',
-        'price' => 'séance d\'essai 10€',
-        'link' => 'https://ccn-orleans-reservations.mapado.com/event/orleans/dancing-kids-avec-anne-perbal-inscription',
-            ],
-        ['public' => 'Pour les adultes',
-            'image' => '../assets/images/kizumba.jpg',
-            'title' => 'Cours de danse',
-            'recurrence' => 'Du 21 septembre 2019 au 25 janvier 2020',
-            'price' => 'séance d\'essai gratuite',
-        'link' => 'https://ccn-orleans-reservations.mapado.com/event/orleans/dancing-kids-avec-anne-perbal-inscription',
-            ],
-        ['public' => 'Pour Tous',
-            'image' => '../assets/images/@kiki-papadopoulou.jpg',
-            'title' => 'Brunch',
-            'recurrence' => 'Vendredi 8 novembre, 19h, au CCNO',
-            'price' => '10€ / 8€, sur réservation',
-        'link'=>'https://ccn-orleans-reservations.mapado.com/event/orleans/dancing-kids-avec-anne-perbal-inscription',
-            ],
-        ];
+        //Le type 0 correspond aux cours et 1 aux formations.
 
-        return $this->twig->render('Activity/course.html.twig', [
-            'activities' => $activities,
-        ]);
-    }
+        $activityManager = new ActivityManager();
 
-    public function formation()
-    {
-        $activities = [
-        ['public' => 'Pour les professionnels',
-            'image' => '../assets/images/stage.jpg',
-            'title' => 'Stage',
-            'recurrence' => 'du lundi 4 au vendredi 8 novembre 2019',
-            'price' => 'Au CCNO',
-            'link' => 'https://ccn-orleans-reservations.mapado.com/',
-        ],
-        ['public' => 'Pour les jeunes',
-            'image' => '../assets/images/stage_jeunes.jpg',
-            'title' => 'immertion danses afro americaines',
-        'recurrence' => 'Du 18 octobre au 25 actobre 2020',
-        'price' => '50€ le weekend',
-        'link'=>'https://ccn-orleans-reservations.mapado.com/event/orleans/dancing-kids-avec-anne-perbal-inscription',
-            ],
-        ];
+        if ($type === 0 || $type === 1) {
+            $activities = $activityManager->selectActivityByType($type);
+        }
 
-        return $this->twig->render('Activity/formation.html.twig', [
-            'activities' => $activities,
+        switch ($type) {
+            case 0:
+                $isCourse = true;
+                break;
+            case 1:
+                $isFormation = true;
+                break;
+        }
+
+        return $this->twig->render('Activity/index.html.twig', [
+            'activities' => $activities ?? '',
+            'isCourse' => $isCourse ?? '',
+            'isFormation' => $isFormation ?? '',
         ]);
     }
 }
