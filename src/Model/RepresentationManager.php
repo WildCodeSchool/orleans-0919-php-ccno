2cb7c39c25d467813df318e034a6d9ffbf607080
wilder <anthonykwanchung@gmail.com>
2019-11-19 16:13:01 +0100

    add delete representation

--- src/Model/RepresentationManager.php
+++ src/Model/RepresentationManager.php
@@ -21,4 +21,11 @@ class RepresentationManager extends AbstractManager
 
         return $statement->fetchAll();
     }
+
+    public function delete(int $id): void
+    {
+        $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE id=:id");
+        $statement->bindValue('id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+    }
 }
